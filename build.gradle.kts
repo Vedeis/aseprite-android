495ad9fbc4e4110eda8bd248a4894e7b0deed632
Viedie <jibrilviedie55@gmail.com>
2026-06-24 19:53:19 +0700

    Add files via upload

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,3 +1,16 @@
 plugins {
-    id("com.android.application") version "8.7.3" apply false
+    id("com.android.application")
+}
+
+android {
+    namespace = "org.aseprite.androidproto"
+    compileSdk = 35
+
+    defaultConfig {
+        applicationId = "org.aseprite.androidproto"
+        minSdk = 23
+        targetSdk = 35
+        versionCode = 1
+        versionName = "1.3.17.2-native-proto"
+    }
 }
